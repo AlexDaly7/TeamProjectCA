14f136a2651297b64dbd4221c880cff0359156ce
ImDarkTom <darktomg21@gmail.com>
2026-02-20 15:52:36 +0000

    feat: basic auth

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useAuth } from '~/composables/auth';
+
+const auth = useAuth();
+</script>
+
+<template>
+    <div>
+        <button @click="auth.signInWithGitHub">
+            Sign in with GitHub
+        </button>
+        <span>{{ auth.user.value?.email }}</span>
+    </div>
+</template>
