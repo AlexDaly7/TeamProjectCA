6a0f195452f2bb6dc4a05ed30b1abf0e5456e9a3
ImDarkTom <darktomg21@gmail.com>
2026-02-23 10:46:01 +0000

    dx: settings

--- app/pages/project.vue
+++ app/pages/project.vue
@@ -1,14 +1,15 @@
-<script setup>
-    import { Timeline } from 'vue-timeline-chart';
-    import "vue-timeline-chart/style.css";
-    const items = [
-        {id: "item1", group: "group1", type: "point", start: 1705878000000 },
-        {id: "item2", group: "group1", type: "point", start: 1705858000000 }
-    ]   
-    const groups = [
-        {id:"group1", label: "Group 1"},
-        {id:"group2", label: "Group 2"}
-    ]
+<script setup lang="ts">
+import { Timeline, type TimelineItem, type TimelineGroup } from 'vue-timeline-chart';
+import "vue-timeline-chart/style.css";
+
+const items: TimelineItem[] = [
+    {id: "item1", group: "group1", type: "point", start: 1705878000000 },
+    {id: "item2", group: "group1", type: "point", start: 1705858000000 }
+]   
+const groups: TimelineGroup[] = [
+    {id:"group1", label: "Group 1"},
+    {id:"group2", label: "Group 2"}
+]
 </script>
 
 <template>
@@ -16,10 +17,8 @@
         <button>Create Task.</button>
     </div>
     <Timeline
-        :items="items"
-        :groups="groups"
+        :items
+        :groups
         :minViewportDuration="50000000"
-        :viewport-min="1740051756"
-    />
-
+        :viewport-min="1740051756" />
 </template>
