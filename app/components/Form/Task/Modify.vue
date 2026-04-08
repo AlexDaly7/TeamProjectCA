1ab3f2a1f6da7774a6ff81a67458298b696d074e
ImDarkTom <darktomg21@gmail.com>
2026-04-08 01:47:53 +0100

    refactor: move form outside drawer

---
+++ app/components/Form/Task/Modify.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+import { ClientModifyTask, type ClientModifyTaskSchema } from '~~/shared/validation';
+import type { TimelineItemWithData } from '~/utils/types/timeline';
+
+const props = defineProps<{
+    selectedTask: TimelineItemWithData | null
+}>();
+
+const { modifyTask } = useCurrentProject();
+
+
+const { handleSubmit, errors, meta, setErrors, setValues } = useForm({
+    validationSchema: toTypedSchema(ClientModifyTask),
+});
+
+const { isLoading, submitHandler } = useEditDialogForm({ meta, handleSubmit, setErrors });
+
+const onSubmit = submitHandler(
+    async (values) => {
+        if (!props.selectedTask) return { error: true, message: 'No selected task.' };
+
+        const payload: ClientModifyTaskSchema = {
+            title: values.title,
+            description: values.description,
+            order: values.order,
+            progress: values.progress,
+            parentId: values.parentId,
+            dateRange: values.dateRange,
+        };
+
+        await modifyTask(props.selectedTask.data.id, payload);
+        return { error: false, data: 'printed' };
+    }, 
+    async (data) => {
+        console.log('on success', data);
+    }
+);
+
+// When selected task changes, update form values
+watch(
+    () => props.selectedTask, 
+    (newTask) => {
+    if (!newTask?.data) return;
+
+    const { title, description, startTime, endTime } = newTask.data;
+
+    setValues({
+        title,
+        description: description ?? '',
+        dateRange: {
+            start: new Date(startTime),
+            end: new Date(endTime),
+        },
+    });
+}, { immediate: true });
+
+</script>
+
+<template>
+    <form 
+        class="flex flex-col pt-2" 
+        @submit.prevent="onSubmit">
+        <label for="title">
+            <span class="font-medium">Title</span>
+            <AppInput 
+                name="title"
+                placeholder="Task Name"
+                :error="errors['title']"
+                :disabled="isLoading" />
+            <ErrorMessage 
+                name="title"
+                class="text-sm text-danger-txt" />
+        </label>
+
+        <label for="description">
+            <span class="font-medium">Description</span>
+            <AppInput 
+                name="description"
+                class="min-h-32 py-2"
+                placeholder="Enter a description for your task here..."
+                as="textarea"
+                :error="errors['description']"
+                :disabled="isLoading" />
+            <ErrorMessage 
+                name="description" 
+                class="text-sm text-danger-txt" />
+        </label>
+
+        <label for="dateRange">
+            <span class="font-medium">Date Range</span>
+            <DatePickerField 
+                name="dateRange"
+                :error="errors['description']"
+                :disabled="isLoading" />
+            <ErrorMessage 
+                name="dateRange" 
+                class="text-sm text-danger-txt" />
+        </label>
+
+        <div class="flex justify-end mt-2">
+            <AppButton
+                type="submit"
+                :loading="isLoading">
+                <div class="inline-flex items-center gap-2">
+                    <Icon name="hugeicons:floppy-disk" />
+                    Save
+                </div>
+            </AppButton>
+        </div>
+    </form>
+</template>
--- app/components/Project/Drawer.vue
+++ app/components/Project/Drawer.vue
@@ -1,60 +1,17 @@
 <script setup lang="ts">
-import type { TimelineItemWithData } from '~/utils/types/timeline';
-import { type ClientModifyTaskSchema, ClientModifyTask } from '~~/shared/validation'
 import type { ActionButtonResult } from '~/utils/types/actionButton';
+import type { TimelineItemWithData } from '~/utils/types/timeline';
 
 const props = defineProps<{
     selectedTask: TimelineItemWithData | null
 }>()
 
-const { 
-    modifyTask,
+const {
+    currentProject,
     deleteTask: deleteTaskHelper,
-    currentProject
 } = useCurrentProject();
 
 const isOpen = defineModel('isOpen', { default: false });
-
-watch(isOpen, () => {
-    const newTask = props.selectedTask;
-    if (!newTask) return;
-
-    setValues({
-        title: newTask.data.title,
-        description: newTask.data.description ?? '',
-        dateRange: {
-            start: new Date(newTask.data.startTime),
-            end: new Date(newTask.data.endTime),
-        },
-    });
-})
-
-const { handleSubmit, errors, meta, setErrors, setValues } = useForm({
-    validationSchema: toTypedSchema(ClientModifyTask),
-});
-
-const { isLoading, submitHandler, submitError } = useEditDialogForm({ meta, handleSubmit, setErrors });
-
-const onSubmit = submitHandler(
-    async (values) => {
-        if (!props.selectedTask) return { error: true, message: 'No selected task.' };
-
-        const payload: ClientModifyTaskSchema = {
-            title: values.title,
-            description: values.description,
-            order: values.order,
-            progress: values.progress,
-            parentId: values.parentId,
-            dateRange: values.dateRange,
-        };
-
-        await modifyTask(props.selectedTask.data.id, payload);
-        return { error: false, data: 'printed' };
-    }, 
-    async (data) => {
-        console.log('on success', data);
-    }
-);
 
 async function deleteTask(): Promise<ActionButtonResult> {
     if (!props.selectedTask) return { error: true, message: 'No selected task.' };
@@ -81,55 +38,7 @@ async function deleteTask(): Promise<ActionButtonResult> {
         </div>
         <div v-else class="h-full p-4 flex flex-col">
             <span class="text-sm font-medium">Selected task:</span>
-            <form class="flex flex-col pt-2" @submit.prevent="onSubmit">
-                <label for="title">
-                    <span class="font-medium">Title</span>
-                    <AppInput 
-                        name="title"
-                        placeholder="Task Name"
-                        :error="errors['title']"
-                        :disabled="isLoading" />
-                    <ErrorMessage 
-                        name="title"
-                        class="text-sm text-danger-txt" />
-                </label>
-
-                <label for="description">
-                    <span class="font-medium">Description</span>
-                    <AppInput 
-                        name="description"
-                        class="min-h-32 py-2"
-                        placeholder="Enter a description for your task here..."
-                        as="textarea"
-                        :error="errors['description']"
-                        :disabled="isLoading" />
-                    <ErrorMessage 
-                        name="description" 
-                        class="text-sm text-danger-txt" />
-                </label>
-
-                <label for="dateRange">
-                    <span class="font-medium">Date Range</span>
-                    <DatePickerField 
-                        name="dateRange"
-                        :error="errors['description']"
-                        :disabled="isLoading" />
-                    <ErrorMessage 
-                        name="dateRange" 
-                        class="text-sm text-danger-txt" />
-                </label>
-
-                <div class="flex justify-end mt-2">
-                    <AppButton
-                        type="submit"
-                        :loading="isLoading">
-                        <div class="inline-flex items-center gap-2">
-                            <Icon name="hugeicons:floppy-disk" />
-                            Save
-                        </div>
-                    </AppButton>
-                </div>
-            </form>
+            <FormTaskModify :selected-task="selectedTask" />
 
             <div class="h-0.5 w-full bg-main-50/10 my-4"></div>
             <AppButton
