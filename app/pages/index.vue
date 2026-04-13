36c22be2b8efc324921c3ce33a60bac7013aef93
ImDarkTom <darktomg21@gmail.com>
2026-04-13 03:52:04 +0100

    feat: landing page format + footer

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -25,8 +25,8 @@ function signIn() {
 </script>
 
 <template>
-    <div class="h-full w-full max-w-6xl mx-auto flex flex-col items-center justify-center scroll-smooth">
-        <div 
+    <main class="h-full w-full max-w-6xl mx-auto flex flex-col items-center justify-center scroll-smooth px-4">
+        <nav 
             class="fixed top-0 left-1/2 -translate-x-1/2 flex flex-row gap-0.5 z-10
             bg-main-800 ring-md rounded-full p-1 mt-2 shadow-sm shadow-black text-sm font-semibold">
             <RouterLink 
@@ -51,51 +51,62 @@ function signIn() {
                 class="rounded-full p-2 px-4 hover:bg-main-700 transition-colors duration-75">
                 GitHub ↗
             </a>
-        </div>
-        <div class="w-2xl mt-48 mb-2">
+        </nav>
+
+        <section class="w-full max-w-2xl mt-48 text-center">
             <h1 id="hero" class="sr-only absolute top-0">Mórchlár</h1>
+            
             <img
                 src="/media/landing/logo.webp"
                 width="1909"
                 height="380" 
                 alt="Mórchlár"
                 fetchpriority="high"
-                class="size-full object-cover object-center drop-shadow-md drop-shadow-black"
+                class="w-full drop-shadow-md drop-shadow-black"
                 data-testid="logo">
-        </div>
-        <p class="text-txt-secondary mb-4">Collaborative team project tracking with GitHub integration.</p>
-        <AppButton 
-            class="inline-flex gap-2 items-center"
-            :disabled="isSigningIn"
-            @click="signIn">
-            <LoadingIcon v-if="isSigningIn" />
-            <Icon
-                v-else
-                name="hugeicons:github-01"
-                size="20"/>
-            Sign in with GitHub
-        </AppButton>
 
-        <h2 
-            id="features"
-            class="text-3xl font-bold mt-8">Features!</h2>
-        <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 m-4">
-            <LandingPageCard 
-                title="Organisations"
-                description="Create your organisation and give it a custom link"
-                video-src="/media/landing/Organisations.webm" />
-            <LandingPageCard 
-                title="Add members"
-                description="Collaborate with your teammates within the organisation"
-                video-src="/media/landing/Adding-Members.webm" />
-            <LandingPageCard 
-                title="GitHub Integration"
-                description="Import your repos directly into the website"
-                video-src="/media/landing/Project.webm" />
-            <LandingPageCard 
-                title="Add Tasks and Subtasks"
-                description="Add tasks and subtasks to our Gannt chart and see how they link"
-                video-src="/media/landing/Tasks-and-Subtasks.webm" />
-        </div>
-    </div>
+            <p class="text-txt-secondary mt-2 mb-4">
+                Collaborative team project tracking with GitHub integration.
+            </p>
+
+            <AppButton 
+                class="inline-flex gap-2 items-center"
+                :disabled="isSigningIn"
+                @click="signIn">
+                <LoadingIcon v-if="isSigningIn" />
+                <Icon
+                    v-else
+                    name="hugeicons:github-01"
+                    size="20"/>
+                Sign in with GitHub
+            </AppButton>
+        </section>
+
+        <section >
+            <h2 id="features" class="text-3xl font-bold mt-8 mb-4 text-center">Features!</h2>
+
+            <div class="grid gap-4 grid-cols-1 sm:grid-cols-2">
+                <LandingPageCard 
+                    title="Organisations"
+                    description="Create your organisation and give it a custom link"
+                    video-src="/media/landing/Organisations.webm" />
+                <LandingPageCard 
+                    title="Add members"
+                    description="Collaborate with your teammates within the organisation"
+                    video-src="/media/landing/Adding-Members.webm" />
+                <LandingPageCard 
+                    title="GitHub Integration"
+                    description="Import your repos directly into the website"
+                    video-src="/media/landing/Project.webm" />
+                <LandingPageCard 
+                    title="Add Tasks and Subtasks"
+                    description="Add tasks and subtasks to our Gannt chart and see how they link"
+                    video-src="/media/landing/Tasks-and-Subtasks.webm" />
+            </div>
+        </section>
+    </main>
+
+    <footer class="text-center text-sm text-txt-secondary py-6">
+        © {{ new Date().getFullYear() }} Mórchlár
+    </footer>
 </template>
