84ae1cfd7802198109e63bb57510c0c003106586
ImDarkTom <darktomg21@gmail.com>
2026-04-15 19:28:36 +0100

    test: make e2e use dev server

--- test/e2e/landing.test.ts
+++ test/e2e/landing.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { createPage, setup, url } from '@nuxt/test-utils/e2e';
 
 describe('landing page', async () => {
-    await setup();
+    await setup({ dev: true });
 
     it('displays the hero image', async () => {
         const page = await createPage();
