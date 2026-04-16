f3b6d7536c98f839fca796eb05cbf4e5f44ea2fc
Refired <alexshepherdno1@gmail.com>
2026-04-16 16:15:39 +0100

    Small commit of test attempts
    Some contain useful code snippets so need to be available at the moment.

---
+++ test/unit/auth.test.ts
@@ -0,0 +1,22 @@
+import { test, describe, vi, beforeEach,  } from 'vitest';
+import { auth } from '../../server/lib/auth/auth';
+import * as authObj from '../../server/middleware/auth';
+
+vi.mock(import('~~/server/lib/auth/auth'),()=> {
+    return {
+        authObj
+    }
+});
+
+describe("/server/utils/auth.ts", ()=> {
+    beforeEach(()=> {
+        console.log("FUCKYOU");
+    })
+
+    test("AGH", ()=> {
+        console.log(authObj);
+
+        
+        expect().toStrictEqual();
+    });
+});
